6c4e2deb3fb87bbd5e1abd429ba2163ef435f2b4
Superbal21 <wju246079@gmail.com>
2024-08-13 09:53:16 +0800

    * 串聯 ChatGPT & LineBot & 氣象資料開放平台

--- Weather.py
+++ Weather.py
@@ -10,8 +10,8 @@ from linebot.v3.messaging import (Configuration,ApiClient,MessagingApi,ReplyMess
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-url = 'https://invoice.etax.nat.gov.tw/index.html'
-web = requests.get(url)
+Invoice_url = 'https://invoice.etax.nat.gov.tw/index.html'
+web = requests.get(Invoice_url)
 web.encoding='utf-8'
 
 soup = BeautifulSoup(web.text, "html.parser")
@@ -19,19 +19,25 @@ td = soup.select('.container-fluid')[0].select('.etw-tbiggest')
 ns = td[0].getText()  # 特別獎
 n1 = td[1].getText()  # 特獎
 
+weather_element_name = {
+    'Wx'  : '天氣現象',
+    'PoP' : '降雨機率',
+    'CI'  : '舒適程度',
+    'MinT': '最低溫度',
+    'MaxT': '最高溫度'
+}
 
 app = Flask(__name__)
-key = os.getenv('Chat_key',None)
+Chat_key = os.getenv('Chat_key',None)
+CWA_key = os.getenv('CWA_key',None)
 Secret = os.getenv('Linebot_Secret', None)
 Token = os.getenv('Linebot_Token',None)
 line_bot_api = LineBotApi(Token)
 line_handler = WebhookHandler(Secret)
 config = Configuration(access_token=Token)
 
-url ='https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-E5DD6503-6C55-4E4E-BB83-ABADD30BE3AA&format=JSON'
+CWA_url ='https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=' + str(CWA_key) + '&format=JSON'
 
-
-ansA=[]
 city=''
 
 @app.route('/')
@@ -52,53 +58,66 @@ def callback():
         abort(400)
     return 'OK'
 
-
-def myG(aa):
-    ansA.clear()
+def City(input):
     ssl._create_default_https_context = ssl._create_unverified_context
-    response = urlopen(url)
+    response = urlopen(CWA_url)
     data = response.read()
-    output = json.loads(data)
-    location=output['records']['location']
-    for i in location:
-        city = i['locationName']
-        if city==aa:
-            wx = i['weatherElement'][0]['time'][0]['parameter']['parameterName']
-            maxtT = i['weatherElement'][4]['time'][0]['parameter']['parameterName']
-            mintT = i['weatherElement'][2]['time'][0]['parameter']['parameterName']
-            ci = i['weatherElement'][3]['time'][0]['parameter']['parameterName']
-            pop = i['weatherElement'][4]['time'][0]['parameter']['parameterName']
+    weather_data = json.loads(data)
+
+    Forecasts = dict()
+    # 每一個location
+    for location in weather_data['records']['location']:
+        # print(location['locationName']) 城市名稱
+        city_name = location['locationName']
+
+        Forecast = dict()
+        # 每一種天氣預報的數值, eg: CI, Wx, PoP, MinT, MaxT
+        for element in location['weatherElement']:
+            # print(element['elementName'], end=': ') 數值的名稱, 結尾用冒號。
+            # print(element['time'][0]['parameter']['parameterName']) 取時間最靠近的數值，所以取index 0。
             
-            ansA.append(city)
-            ansA.append(wx)
-            ansA.append(mintT)
-            ansA.append(maxtT)
-    return ansA
+            # 取得資訊
+            element_name = element['elementName'] # 英文名稱
+            element_value = element['time'][0]['parameter']['parameterName']
+            if element_name in ['MinT', 'MaxT']:
+                element_unit = '°C'
+            elif element_name in ['PoP']:
+                element_unit = '%'
+            else:
+                element_unit = ''
+
+            # 轉成對應的中文名稱
+            element_name = weather_element_name[element_name]
+            Forecast[element_name] = element_value + element_unit
+
+        Forecasts[city_name] = Forecast
+    return Forecasts
 
 
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):        
+    userID = event.source.user_id #傳訊息的人的ID
 
     if event.message.text == "1":        
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=str(myG('臺北市'))))        
+            TextSendMessage(text=str(City('臺北市'))))        
 
     elif event.message.text == "2":        
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=str(myG('桃園市'))))
+            TextSendMessage(text=str(City('桃園市'))))
         
     elif event.message.text=='3':            
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=str(myG('新竹市'))))
+            TextSendMessage(text=str(City('新竹市'))))
         
     elif event.message.text=='4':            
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=str(myG('臺中市'))))
+            TextSendMessage(text=str(City('臺中市'))))
         
     elif event.message.text=='5':            
         line_bot_api.reply_message(
@@ -111,10 +130,10 @@ def handle_message(event):
             TextSendMessage(text='頭獎: '+str(n1)))
         
     else:
-        if key and event.message.text:
+        if Chat_key and event.message.text:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=chat(event.message.text,key)))
+                TextSendMessage(text=chat(event.message.text,Chat_key,userID)))
         else:
             line_bot_api.reply_message(
                 event.reply_token,
--- test__openai.py
+++ test__openai.py
@@ -1,25 +1,51 @@
 from openai import OpenAI
+from pprint import pprint
 import os
 
-def chat(message,key):
+chat_history = dict()
+
+def chat(message,key,userID):
     client = OpenAI(api_key = key)
+
+    # 把使用者的對話訊息加到紀錄中
+    if userID in chat_history:
+        chat_history[userID].append({"role": "user","content": message})
+    else:
+        chat_history[userID] = [{"role":"user","content": message}]
+
+    message = message + '' # 可以改變ChatGPT的語氣
     chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": message,
-            }
-        ],
-        model="gpt-3.5-turbo",
+        messages = chat_history[userID][:-1] + [{"role": "user", "content": message}],
+        model = "gpt-3.5-turbo",
     )
 
     response = chat_completion.choices[0].message.content
+
+    # 把ChatGPT的對話訊息加到紀錄中
+    chat_history[userID].append(
+        {
+            "role": "system",
+            "content": response,
+        }
+    )
     return response if response else 'Error'
 
 if __name__ == '__main__':
-    message = "Hello my name is Helios :-)"
     key = os.getenv('Chat_key',None)
 
-    if message and key:
-        reponse = chat(message,key)
-        print(reponse)
+    while True:
+        message = input("Enter something: ")
+        userID = 'Helios'
+
+        if message == 'Quit':
+            print("Chat ended.")
+            break
+
+        if message and key:
+            reponse = chat(message,key,userID)
+            print("ChatGPT say: ", reponse)
+        else:
+            print("API key error, ", key)
+
+        print("History: ")
+        pprint(chat_history)
